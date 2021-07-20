44f4ed11dbf6b3a6ad5077e1e9028cdfcf695796
kolawole Olulana <kolawoleolulana@kolawoles-MacBook-Air.local>
2021-07-20 13:47:09 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
+
+const Button =() => {
+  const [counter, setCounter ] = useState(0)
+
+  return (
+    <div onClick={ ()=>{ setCounter((c)=>c+1)}}>I am a button {counter}</div>
+
+  )
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
